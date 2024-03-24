40fc1c8974ddfe4b112db968fb4cb974d5656a62
w-winiarz <winiarzwojtek@gmail.com>
2024-03-24 11:23:51 +0100

    LAB4

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+a = np.array([0, 1])
+print(a)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+a = np.arange(2)
+print(a)
+print(type(a))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print(a.dtype)
+
+
+a = np.arange(2, dtype='int32')
+print(a.dtype)
+b = a.astype('float')
+print(b)
